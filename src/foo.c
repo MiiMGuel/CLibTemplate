1fc9ba3f0f531269e6e592541ed74e94983ab2c5
migel <miigel065@gmail.com>
2024-06-22 18:21:51 +0700

    typing fixed

--- src/foo.c
+++ src/foo.c
@@ -5,7 +5,7 @@ i32 foo_sum(i32 x, i32 y) { return x + y; }
 i32 foo_sub(i32 x, i32 y) { return x - y; }
 i32 foo_mul(i32 x, i32 y) { return x * y; }
 i32 foo_div(i32 x, i32 y) { return x / y; }
-i32 FAPI foo_version_major(void) { return foo_VERSION_MAJOR; }
-i32 FAPI foo_version_minor(void) { return foo_VERSION_MINOR; }
-i32 FAPI foo_version_patch(void) { return foo_VERSION_PATCH; }
-i32 FAPI foo_version_tweak(void) { return foo_VERSION_TWEAK; }
+i32 foo_version_major(void) { return foo_VERSION_MAJOR; }
+i32 foo_version_minor(void) { return foo_VERSION_MINOR; }
+i32 foo_version_patch(void) { return foo_VERSION_PATCH; }
+i32 foo_version_tweak(void) { return foo_VERSION_TWEAK; }
