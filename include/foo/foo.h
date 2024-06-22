0cb50428920f30b4345d26d799dd99c174add792
migel <miigel065@gmail.com>
2024-06-22 10:20:02 +0700

    init

---
+++ include/foo/foo.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int foo_sum(int x, int y);
+int foo_sub(int x, int y);
+int foo_mul(int x, int y);
+int foo_div(int x, int y);
---
+++ src/foo.c
@@ -0,0 +1,4 @@
+int foo_sum(int x, int y) { return x + y; }
+int foo_sub(int x, int y) { return x - y; }
+int foo_mul(int x, int y) { return x * y; }
+int foo_div(int x, int y) { return x / y; }
