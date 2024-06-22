9fdf16a8c5c5725ca465c67030a91494e8d3722d
migel <miigel065@gmail.com>
2024-06-22 15:58:48 +0700

    var type size assertions

--- include/types.h
+++ include/types.h
@@ -17,11 +17,11 @@ typedef float              f32;
 typedef double             f64;
 
 // boolean types
-typedef int                b32;
 typedef char               b8;
+typedef int                b32;
 
 // boolean values
-#define TRUE 1
+#define TRUE  1
 #define FALSE 0
 
 // assertions
@@ -30,6 +30,20 @@ typedef char               b8;
 #else // __clang__ || __gcc__
 #   define STATIC_ASSERT static_assert
 #endif // __clang__ || __gcc__
+
+// assert sizes
+STATIC_ASSERT(sizeof(u8)  == 1, "size of u8 is not 1 bytes!");
+STATIC_ASSERT(sizeof(u16) == 2, "size of u16 is not 2 bytes!");
+STATIC_ASSERT(sizeof(u32) == 4, "size of u32 is not 4 bytes!");
+STATIC_ASSERT(sizeof(u64) == 8, "size of u64 is not 8 bytes!");
+STATIC_ASSERT(sizeof(i8)  == 1, "size of i8 is not 1 bytes!");
+STATIC_ASSERT(sizeof(i16) == 2, "size of i16 is not 2 bytes!");
+STATIC_ASSERT(sizeof(i32) == 4, "size of i32 is not 4 bytes!");
+STATIC_ASSERT(sizeof(i64) == 8, "size of i64 is not 8 bytes!");
+STATIC_ASSERT(sizeof(f32) == 4, "size of f32 is not 4 bytes!");
+STATIC_ASSERT(sizeof(f64) == 8, "size of f64 is not 8 bytes!");
+STATIC_ASSERT(sizeof(b8)  == 1, "size of b8 is not 1 bytes!");
+STATIC_ASSERT(sizeof(b32) == 4, "size of b32 is not 4 bytes!");
 
 // API export / import
 #ifdef FOO_EXPORT
