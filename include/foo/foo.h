dc30c9ee0dae3afba1800d0dfeb467fe24b919d4
migel <miigel065@gmail.com>
2024-06-22 12:30:11 +0700

    maybe?

--- include/foo/foo.h
+++ include/foo/foo.h
@@ -1,6 +1,24 @@
 #pragma once
 
-int foo_sum(int x, int y);
-int foo_sub(int x, int y);
-int foo_mul(int x, int y);
-int foo_div(int x, int y);
+#ifdef FOO_EXPORT
+#   if defined(_WIN32) || defined(_WIN64)
+#       define FAPI __declspec(dllexport)
+#   else
+#       define FAPI __attribute__((visibility("default")))
+#   endif
+#else
+#   if defined(_WIN32) || defined(_WIN64) 
+#       if defined(__clang__) || defined(__GNUC__)
+#           define FAPI 
+#       else 
+#           define FAPI __declspec(dllimport)
+#       endif
+#   else 
+#       define FAPI
+#   endif
+#endif 
+
+int FAPI foo_sum(int x, int y);
+int FAPI foo_sub(int x, int y);
+int FAPI foo_mul(int x, int y);
+int FAPI foo_div(int x, int y);
--- src/foo.c
+++ src/foo.c
@@ -1,3 +1,5 @@
+#include "foo.h"
+
 int foo_sum(int x, int y) { return x + y; }
 int foo_sub(int x, int y) { return x - y; }
 int foo_mul(int x, int y) { return x * y; }
