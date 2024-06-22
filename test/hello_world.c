dfd4a7329039c2aeb55d2b268c1334a0961eee2a
migel <miigel065@gmail.com>
2024-06-22 15:07:34 +0700

    test

---
+++ test/hello_world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "foo/foo.h"
+
+int main(void) {
+    printf("Hello, World! \n");
+    printf("sum(5, 2) : %d \n", foo_sum(5, 2));
+    printf("sub(5, 2) : %d \n", foo_sub(5, 2));
+    printf("mul(5, 2) : %d \n", foo_mul(5, 2));
+    printf("div(5, 2) : %d \n", foo_div(5, 2));
+
+    return 0;
+}
